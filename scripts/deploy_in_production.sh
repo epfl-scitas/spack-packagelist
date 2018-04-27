84882d1949fffd92ac744a10b580fce69d42d644
culpo <massimiliano.culpo@gmail.com>
2018-04-27 08:55:59 +0200

    Empty list of specs to be installed shouldn't make CD fail

--- scripts/deploy_in_production.sh
+++ scripts/deploy_in_production.sh
@@ -11,4 +11,14 @@
 spack --version
 
 # Install the software that is missing
-spack install --log-file=spec.${SPACK_TARGET_TYPE}.xml --log-format=junit $(cat to_be_installed.${SPACK_TARGET_TYPE}.txt)
+
+specs_to_be_installed=$(cat to_be_installed.${SPACK_TARGET_TYPE}.txt)
+
+if [[ -z "${specs_to_be_installed}" ]]
+then
+    echo "[${SPACK_TARGET_TYPE}] Nothing to install"
+else
+    spack install --log-file=spec.${SPACK_TARGET_TYPE}.xml --log-format=junit ${specs_to_be_installed}
+fi
+
+
