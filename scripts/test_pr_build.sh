5a71e2890b1ec4c5cca46b40d278757f657c92ad
culpo <massimiliano.culpo@gmail.com>
2018-04-30 23:02:45 +0200

    It shouldn't be needed to load paien

--- scripts/test_pr_build.sh
+++ scripts/test_pr_build.sh
@@ -9,7 +9,6 @@
 SPACK_CHECKOUT_DIR=$(cat spack_dir.txt)
 
 # Try to install the specs
-slmodules -r paien
 . ${SPACK_CHECKOUT_DIR}/share/spack/setup-env.sh
 
 # We need to install Intel to have the intel module available
