47371a87508fa2ccda0278565fe8c8dd18a45dcf
Massimiliano Culpo <massimiliano.culpo@gmail.com>
2018-05-01 06:48:58 +0200

    Added recipe for fftw+mpi (#42)

--- scripts/test_pr_build.sh
+++ scripts/test_pr_build.sh
@@ -10,6 +10,7 @@ SPACK_CHECKOUT_DIR=$(cat spack_dir.txt)
 
 # Try to install the specs
 . ${SPACK_CHECKOUT_DIR}/share/spack/setup-env.sh
+
 # We need to install Intel to have the intel module available
 # in the temporary workspace
 spack install intel@18.0.2 %gcc@4.8.5
