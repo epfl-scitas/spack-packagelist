b655fec09ac57d7851b6fb446e4c3e8608fe0546
Massimiliano Culpo <massimiliano.culpo@gmail.com>
2018-05-01 14:02:09 +0200

    Added sbt version 1.1.4 to Core (#43)
    * Added sbt version 1.1.4 to Core
    
    * Fixed query of installed specs. Create mirror with the spack under PR.
    
    Spack apparently has a bug in the string representation of Specs, so we
    need to force SPACK_TARGET_TYPE to be the target we are querying if we
    don't want false positives with mixed targets in concretized the spec.

--- scripts/setup_pr_configuration.sh
+++ scripts/setup_pr_configuration.sh
@@ -55,30 +55,46 @@ which spack
 . ${SENV_VIRTUALENV_PATH}/bin/activate
 senv --help
 # Generate the list of software that need to be installed, then fetch every tarball
-for target in $(senv targets)
+
+# For this part we currently need to work with the production instance of Spack,
+# as we need to query which software was already installed
+targets="$(senv targets)"
+original_arch="${SPACK_TARGET_TYPE}"
+
+for target in ${targets}
 do
     echo "[${target}] Computing list of packages to be tested"
     senv packages ${target} --output="all_specs.${target}.txt"
+
     # TODO: if concretization is slow this command could output also the yaml file
-    spack filter --not-installed $(cat all_specs.${target}.txt) > to_be_installed.${target}.txt
+
+    # FIXME: Spack apparently has a bug in the string representation of Specs,
+    # FIXME: so we need to force SPACK_TARGET_TYPE to be the target we are
+    # FIXME: querying if we don't want false positives with mixed targets in
+    # FIXME: concretized the spec
+    SPACK_TARGET_TYPE="${target}" spack filter --not-installed $(cat all_specs.${target}.txt) > to_be_installed.${target}.txt
     echo "[${target}] $(cat to_be_installed.${target}.txt)"
 
+done
+deactivate
+
+# Add the mirror to the temporary Spack instance
+
+. ${SPACK_CHECKOUT_DIR}/share/spack/setup-env.sh
+which spack
+
+for target in ${targets}
+do
     # TODO: read directly from a yaml file to avoid concretization slowdowns
     echo "[${target}] Populating temporary mirror"
     while read -r line
     do
         echo "spack mirror create -D -d ${SPACK_MIRROR_DIR} ${line}"
-        spack mirror create -D -d ${SPACK_MIRROR_DIR} ${line}
+        SPACK_TARGET_TYPE="${target}" spack mirror create -D -d ${SPACK_MIRROR_DIR} ${line}
 
         # Exit with an error if mirror failed
         if [[ "$?" != 0 ]] ; then exit 1 ; fi
 
     done < to_be_installed.${target}.txt
 done
-
-# Add the mirror to the temporary Spack instance
-deactivate
-
-. ${SPACK_CHECKOUT_DIR}/share/spack/setup-env.sh
-which spack
 spack mirror add --scope=site temp_mirror ${SPACK_MIRROR_DIR}
