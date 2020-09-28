8322210ece5570c6671ed1ebfd7a0c82cf8d16a9
Nicolas Richart <nicolas.richart@epfl.ch>
2020-09-29 00:22:00 +0200

    list-envs is a command not an option

--- jenkins/deploy/scripts/populate_mirror.sh
+++ jenkins/deploy/scripts/populate_mirror.sh
@@ -22,7 +22,7 @@ set -u
 
 GET_ENTRY="senv --input ${STACK_RELEASE}.yaml get-environment-entry"
 SPACK_MIRROR_DIR=$(${GET_ENTRY} spack_root)/$(${GET_ENTRY} mirrors.local)
-environments=$(senv --input ${STACK_RELEASE}.yaml --list-envs)
+environments=$(senv --input ${STACK_RELEASE}.yaml list-envs)
 
 deactivate
 
