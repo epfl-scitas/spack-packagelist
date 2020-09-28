9c25e64bdf7f3e02e0c96e40f6cd80c9138108b9
Nicolas Richart <nicolas.richart@epfl.ch>
2020-09-28 17:04:23 +0200

    Bug fix related to virtualenv

--- jenkins/deploy/scripts/update_production_configuration.sh
+++ jenkins/deploy/scripts/update_production_configuration.sh
@@ -15,6 +15,9 @@ echo '(Re)installing senv'
 mkdir -p ${SENV_VIRTUALENV_PATH}
 virtualenv --version
 virtualenv -p $(which python3) ${SENV_VIRTUALENV_PATH} --clear
+
+set +u # bug fix for virtualenv <16.2
 . ${SENV_VIRTUALENV_PATH}/bin/activate
+
 pip install --force-reinstall -U .
 senv status
