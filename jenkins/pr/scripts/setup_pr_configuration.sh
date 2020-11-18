8aa741b1bb5207510db2f360d45d9629d7e9a001
Nicolas Richart <nrichart@users.noreply.github.com>
2020-11-18 17:23:40 +0100

    Update setup_pr_configuration.sh

--- jenkins/pr/scripts/setup_pr_configuration.sh
+++ jenkins/pr/scripts/setup_pr_configuration.sh
@@ -109,11 +109,11 @@ do
     while read -r line
     do
         echo "spack mirror create -D -d ${SPACK_MIRROR_DIR} ${line}"
-        SPACK_TARGET_TYPE="${target}" spack mirror create -D -d ${SPACK_MIRROR_DIR} ${line}
+        SPACK_TARGET_TYPE="${target}" spack mirror create -D -d ${SPACK_MIRROR_DIR} ${line} || true
 
         # Exit with an error if mirror failed
         if [[ "$?" != 0 ]] ; then exit 1 ; fi
 
     done < to_be_installed.${target}.txt
 done
-spack mirror add --scope=site temp_mirror ${SPACK_MIRROR_DIR} || true
+spack mirror add --scope=site temp_mirror ${SPACK_MIRROR_DIR}
