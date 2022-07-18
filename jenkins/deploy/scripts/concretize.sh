f8f2b152cd6c702949f7ffd874116c525f4b96d2
Nicolas Richart <nicolas.richart@epfl.ch>
2022-07-18 21:08:40 +0200

    debuging concretization

--- jenkins/deploy/scripts/concretize.sh
+++ jenkins/deploy/scripts/concretize.sh
@@ -8,7 +8,7 @@ set +u
 . ${SENV_VIRTUALENV_PATH}/bin/activate
 set -u
 
-SPACK_CHECKOUT_DIR=$(jenkins/senv.sh --input ${STACK_RELEASE}.yaml spack-checkout-dir)
+SPACK_CHECKOUT_DIR=$(jenkins/senv.sh spack-checkout-dir)
 
 if [ x'${DRY_RUN}' = 'xyes' ]; then
     SPACK="echo ${SPACK_CHECKOUT_DIR}/bin/spack"
@@ -16,4 +16,4 @@ else
     SPACK="${SPACK_CHECKOUT_DIR}/bin/spack"
 fi
 
-${SPACK} --env ${environment} concretize
+${SPACK} -v --env ${environment} concretize
